f62e723a6a158e0c2e0ed6f344da6a1becc1eb94
Desant <anturated@gmail.com>
2026-05-17 16:54:09 +0200

    fix: rework vars

---
+++ homes/ivy/compositors/hyprland/vars.nix
@@ -0,0 +1,17 @@
+{ config, lib, ... }:
+
+let
+  inherit (config.ceirios.software) defaults;
+in
+''
+  -- bg stuff
+  local bar = "qs -c ivy -d"
+  local wallpaper = "awww-daemon"
+  local filter = "hyprsunset"
+  local polkit = "soteria"
+
+  -- software defaults
+  local terminal = "${defaults.terminal}"
+  local menu = "rofi -show drun"
+  local browser = "vivaldi"
+''
