893ff2d17d2525f03978b35c706d23c420a2b16b
Desant <desant@anturated.dev>
2026-05-20 16:55:24 +0200

    feat: add acme dns provider stuff

--- modules/nixos/services/lego.nix
+++ modules/nixos/services/lego.nix
@@ -1,13 +1,88 @@
-{ config, lib, ... }:
+{
+  config,
+  lib,
+  self,
+  ...
+}:
 
 let
-  inherit (lib) mkIf;
+  inherit (lib)
+    mkIf
+    mkOption
+    types
+    listToAttrs
+    mapAttrsToList
+    nameValuePair
+    concatLists
+    ;
+  inherit (types) str nullOr;
+
+  inherit (self.lib) mkSecret;
+  inherit (config.sops) secrets;
+
+  # they all need different var names so i'd rather be explicit
+  providerCreds = {
+    porkbun = {
+      PORKBUN_API_KEY_FILE = secrets.lego-porkbun-key.path;
+      PORKBUN_SECRET_API_KEY_FILE = secrets.lego-porkbun-secret.path;
+    };
+  };
+
+  # i love automating things i could've defined once
+  # also this feels like it was written by a 5th grader
+  # TODO: is there a way to skip listToAttrs (concatLists (mapAttrsToList()))?
+  mkLegoSecrets =
+    let
+      mkEntry =
+        provider: type:
+        nameValuePair "lego-${provider}-${type}" (mkSecret {
+          file = "lego";
+          key = "${provider}-${type}";
+          owner = "acme";
+          group = "acme";
+        });
+    in
+    listToAttrs (
+      concatLists (
+        mapAttrsToList (provider: _: [
+          (mkEntry provider "key")
+          (mkEntry provider "secret")
+        ]) providerCreds
+      )
+    );
 in
 {
+  options.security.acme.certs = mkOption {
+    type = types.attrsOf (
+      types.submodule (
+        { config, ... }:
+        {
+          freeformType = types.attrsOf types.anything;
+
+          options.autoProvider = mkOption {
+            type = nullOr str;
+            default = "porkbun";
+            description = "Put your provider here to automatically load secrets for it";
+          };
+
+          config = mkIf (config.autoProvider != null) {
+            dnsProvider = config.autoProvider;
+            credentialFiles =
+              providerCreds.${config.autoProvider}
+                or (throw "Auto creds not configured for ${config.autoProvider}");
+            webroot = null;
+          };
+        }
+      )
+    );
+  };
+
   config = mkIf config.ceirios.fywion.nginx.enable {
     security.acme = {
       acceptTerms = true;
       defaults.email = "desant" + "@" + "anturated" + "." + "dev";
     };
+
+    sops.secrets = mkLegoSecrets;
   };
 }
