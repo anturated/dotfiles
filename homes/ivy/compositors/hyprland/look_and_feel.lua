2f32ffbd7253f5b608c0ff81476066f0bd26add9
Desant <desant@anturated.dev>
2026-05-27 18:03:27 +0200

    feat: disable tearing for now

--- homes/ivy/compositors/hyprland/look_and_feel.lua
+++ homes/ivy/compositors/hyprland/look_and_feel.lua
@@ -16,7 +16,7 @@ hl.config({
     -- we game. this should only affect windows that request tearing.
     -- if this is false, tearing won't happen at all.
     -- https://wiki.hypr.land/Configuring/Advanced-and-Cool/Tearing/
-    allow_tearing = true,
+    allow_tearing = false,
 
     -- binary tree layout
     layout = "dwindle",
