70a904fe5e65612fa55717e309c00223d46c55a6
Desant <desant@anturated.dev>
2026-05-30 05:51:41 +0200

    feat: trust wheel

--- modules/iso/nix.nix
+++ modules/iso/nix.nix
@@ -12,6 +12,10 @@
         "nix-command"
       ];
 
+      # let me mess with the store
+      allowed-users = [ "@wheel" ];
+      trusted-users = [ "@wheel" ];
+
       # more logging is nice when doing installs, we want to know if something goes wrong
       log-lines = 50;
 
