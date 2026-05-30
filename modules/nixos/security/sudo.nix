929b66d1daa4aa0ee3d39ab648970fcbb5f25b16
Desant <desant@anturated.dev>
2026-05-30 07:06:55 +0200

    feat: nasty sudo stuff

--- modules/nixos/security/sudo.nix
+++ modules/nixos/security/sudo.nix
@@ -1,9 +1,19 @@
-{ ... }:
+{ lib, ... }:
 
 {
   security.sudo-rs = {
     enable = true;
 
+    # UHHHH I DON'T LIKE THIS BUT WE'LL SEE HOW IT GOES
+    # this is here because password on rebuild and deploy is annoying
+
+    # disable sudo password prompt for wheel on trusted binaries
+    wheelNeedsPassword = lib.mkDefault false;
+    execWheelOnly = true;
+
+    # disable the here be dragons message
+    # display '*' when entering password
+    # keep some env stuff
     extraConfig = ''
       Defaults !lecture
       Defaults pwfeedback
