5d21b24c373502b54e46fac48ebc147200b3aaa0
Desant <desant@anturated.dev>
2026-06-25 03:07:35 +0200

    chore: note

--- modules/nixos/extras.nix
+++ modules/nixos/extras.nix
@@ -1,6 +1,10 @@
 { inputs, ... }:
 
 {
+  # NOTE TO SELF:
+  # this exists because darwin needs its own import for HM
+  # so until i figure out where to put this, it stays
+
   imports = [
     inputs.home-manager.nixosModules.home-manager
   ];
