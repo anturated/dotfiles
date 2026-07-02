e2712457dbc5d1501aaeae75b9020a99bb663cb5
Desant <desant@anturated.dev>
2026-07-02 13:52:03 +0200

    feat: improve aquamarine

--- homes/ivy/compositors/hyprland/aquamarine.nix
+++ homes/ivy/compositors/hyprland/aquamarine.nix
@@ -1,15 +1,17 @@
 { osConfig, lib, ... }:
 
 let
-  inherit (osConfig.ceirios.hardware) cpu gpu;
+  inherit (osConfig.ceirios.hardware) cpu gpu busIds;
   inherit (lib.strings) optionalString;
 
+  # set hyprland up to recognize monitors wired to other GPUs.
+  # this'll only ever work if we have the cards bound, which requires busids to be set
+  # TODO: this is still not as flexible as i want but will work in most cases
   isHybrid = gpu == "nv-hybrid";
+  hasBusIds = (busIds.primary != null && busIds.discrete != null);
 
-  primary = if isHybrid then "/dev/dri/${cpu}-gpu" else "/dev/dri/${gpu}-gpu";
-  secondary = optionalString isHybrid ":/dev/dri/nvidia-gpu";
-  devices = primary + secondary;
+  devices = "/dev/dri/${cpu}-gpu:/dev/dri/nvidia-gpu";
 
-  env = optionalString (gpu != null) "hl.env(\"AQ_DRM_DEVICES\",\"${devices}\")";
+  env = optionalString (isHybrid && hasBusIds) "hl.env(\"AQ_DRM_DEVICES\",\"${devices}\")";
 in
 env
