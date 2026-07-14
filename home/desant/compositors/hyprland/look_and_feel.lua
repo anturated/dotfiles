75896a450ce123394bfc04ed3361dcc70e5af8e1
Desant <desant@anturated.dev>
2026-07-14 18:06:34 +0200

    feat: disable splash text

--- home/desant/compositors/hyprland/look_and_feel.lua
+++ home/desant/compositors/hyprland/look_and_feel.lua
@@ -53,6 +53,7 @@ hl.config({
   misc = {
     force_default_wallpaper = 1,
     disable_hyprland_logo = true,
+    disable_splash_rendering = true,
   },
 
   -- this feels better, i can't really describe it
