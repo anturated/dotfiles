98669738afa36d0180bc7776cdbd5f841491f702
Desant <desant@anturated.dev>
2026-07-23 17:32:12 +0200

    chore: comments

--- modules/flake/shell.nix
+++ modules/flake/shell.nix
@@ -6,15 +6,18 @@
   nix-output-monitor,
   ffmpeg,
 }:
+
 mkShellNoCC {
   name = "ceirios";
 
   packages = [
+    # we wanna have some form of these to at least bootstrap the flake
     just
     gitMinimal
     sops
-    nix-output-monitor
-    ffmpeg
+
+    nix-output-monitor # for just rebuild and all that
+    ffmpeg # for just gif
   ];
 
   env.DIRENV_LOG_FORMAT = "";
