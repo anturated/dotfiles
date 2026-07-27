9b4ab66f99a561a14392587cc1a65be773097c7f
Desant <desant@anturated.dev>
2026-07-27 19:43:45 +0200

    feat: remove prism overrides

--- home/desant/gaming/prism.nix
+++ home/desant/gaming/prism.nix
@@ -12,19 +12,7 @@ in
 {
   config = mkIf gaming.enable {
     ceirios.packages = {
-      prismlauncher = pkgs.prismlauncher.override {
-        # Add binary required by some mod
-        additionalPrograms = [ pkgs.ffmpeg ];
-
-        # Change Java runtimes available to Prism Launcher
-        jdks = [
-          # graalvm-ce
-          pkgs.graalvmPackages.graalvm-ce
-          pkgs.zulu8
-          pkgs.zulu17
-          pkgs.zulu
-        ];
-      };
+      inherit (pkgs) prismlauncher;
     };
 
     # audio fix
