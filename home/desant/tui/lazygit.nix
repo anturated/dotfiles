ba4bf93b44efaff4b76f2cfae7b91289b2c39757
Desant <desant@anturated.dev>
2026-08-28 16:09:59 +0200

    Revert "fix: disable lazygit config"
    This reverts commit 130f7c877ad069061fc843442472d081cf32b749.

--- home/desant/tui/lazygit.nix
+++ home/desant/tui/lazygit.nix
@@ -3,19 +3,19 @@
     # enable it globally for now just in case
     enable = true;
 
-    # settings = {
-    #   # nixos handles updates
-    #   update.method = "never";
-    #
-    #   git = {
-    #     # allow rewording past commmits
-    #     overrideGpg = true;
-    #
-    #     # use delta pager
-    #     pagers = [
-    #       { pager = "delta --paging=never"; }
-    #     ];
-    #   };
-    # };
+    settings = {
+      # nixos handles updates
+      update.method = "never";
+
+      git = {
+        # allow rewording past commmits
+        overrideGpg = true;
+
+        # use delta pager
+        pagers = [
+          { pager = "delta --paging=never"; }
+        ];
+      };
+    };
   };
 }
