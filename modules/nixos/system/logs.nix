453a9b9650c07ac3de7cde849c393ed80bdcde17
Desant <desant@anturated.dev>
2026-09-05 22:40:55 +0200

    fix: update syntax

--- modules/nixos/system/logs.nix
+++ modules/nixos/system/logs.nix
@@ -7,10 +7,10 @@ in
 {
   services.journald = {
     # lower logs size on non-server machines
-    extraConfig = mkIf (!server.enable) ''
-      SystemMaxUse=100M
-      RuntimeMaxUse=50M
-      SystemMaxFileSize=50M
-    '';
+    settings.Journal = mkIf (!server.enable) {
+      SystemMaxUse = "100M";
+      RuntimeMaxUse = "50M";
+      SystemMaxFileSize = "50M";
+    };
   };
 }
